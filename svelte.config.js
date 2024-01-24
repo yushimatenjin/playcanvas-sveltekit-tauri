084728e3c5deadd9c9b8300f1a82c277bf8b40fc
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2024-01-24 11:26:55 +0900

    ファイル名の変更

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto' // This was changed from adapter-auto
+import adapter from '@sveltejs/adapter-static' // This was changed from adapter-auto
+// tauriの場合は: @sveltejs/adapter-static に変更しました
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
